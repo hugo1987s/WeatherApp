638baf6a92149725f5217c63a0dd1eb12925d100
hugo1987s <hugo.schreiber@hasar.com>
2019-10-01 13:51:45 -0300

    Se quita el contenido default de App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>WeatherApp - Aplicación del Clima</h1>
+     <WeatherLocation></WeatherLocation>
     </div>
+    
   );
 }
+
 
 export default App;
