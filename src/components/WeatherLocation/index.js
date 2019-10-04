82ab7a7613a8312cac9112f5c861c56a0346ef5e
Hugo <hugoschreiber1@gmail.com>
2019-10-03 22:40:00 -0300

    Se agrega CSS de Location

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import './styles.css';
 
 const WeatherLocation = () => (
-    <div>
+    <div className="weatherLocationCont">
         <Location city={"Grand Bourg"}></Location>
         <WeatherData></WeatherData>
     </div>
