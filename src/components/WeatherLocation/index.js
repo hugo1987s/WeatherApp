433ddcc5c279695864c820c7a284a5513c1b2f58
Hugo <hugoschreiber1@gmail.com>
2019-10-04 21:40:47 -0300

    Se agrega fetch a API del clima

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -11,19 +11,72 @@ import {
     WINDY,
 } from './../../constants/weathers';
 
+
+const location = "Buenos Aires,ar";
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
-    temperature: 15,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '140 m/s'
-};
+    temperature : 10,
+    weatherState : SUN,
+    humidity: 5,
+    wind: '10 m/s',
+}
 
 class WeatherLocation extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            city: "Grand Bourg",
+            data: data,
+        };
+    }
+    
+getWeatherState = weather_data => {
+    return SUN;
+}
+
+getData = weather_data => {
+    const { humidity, temp } = weather_data.main;
+    const { speed } = weather_data.wind;
+    const weatherState = SUN;
+
+    const data = {
+        humidity,
+        temperature : temp,
+        weatherState,
+        wind: `${speed} m/s`,
+    }
+
+    return data;
+}
+
+    handleUpdateClick = () => {
+        fetch(api_weather).then( resolve =>{
+            return resolve.json();
+        }).then(data => {
+
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            debugger;
+            this.setState({
+                data: newWeather
+            });
+        });
+
+      
+    }
+
     render() {
+        const { city, data} = this.state;
         return (
              <div className="weatherLocationCont">
-                <Location city={"Grand Bourg"}></Location>
+                <Location city={city}></Location>
                 <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
             </div>
         );
     }
