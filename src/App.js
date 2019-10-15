227d695912a7f48591a85c55422f6c74ecf8df8a
hugo1987s <hugo.schreiber@hasar.com>
2019-10-15 13:25:37 -0300

    Se crea el componente ForecastExtended

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import {Grid, Col, Row } from 'react-flexbox-grid';
-import './App.css';
 import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
 
+import './App.css';
 
 const cities = [
   'Buenos Aires,ar',
@@ -34,7 +35,9 @@ function App() {
       
       <Col xs={12} md={6}>
         <Paper elevation={4}>
-          <div className="details"></div>
+          <div className="details">
+            <ForecastExtended></ForecastExtended>
+          </div>
         </Paper>
         
       </Col>
---
+++ src/components/ForecastExtended.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ForecastExtended extends Component {
+
+    render(){
+        return (<div>Pronóstico extendido</div>);
+    }
+
+};
+
+export default ForecastExtended;
